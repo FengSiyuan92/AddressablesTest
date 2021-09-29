5c9ae1453e3a6b0a7f19344effc5ef891e461d92
TKOFFICE\fengsy01 <fengsy01@mail.jj.cn>
2021-09-29 20:15:37 +0800

    change

---
+++ Assets/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var handler = Addressables.InitializeAsync();
+        handler.WaitForCompletion();
+        Addressables.LoadAssetAsync<GameObject>("Sphere").Completed += (go) => { Instantiate(go.Result); };
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
